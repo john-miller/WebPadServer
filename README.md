5e3f097474d69288f1d652e2821cd94aea54172d
Jonathan Miller <jonmiller44@gmail.com>
2017-01-31 02:05:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WebPadServer
---
+++ src/main/java/io/ess/WebPadServerApplication.java
@@ -0,0 +1,12 @@
+package io.ess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPadServerApplication.class, args);
+	}
+}
