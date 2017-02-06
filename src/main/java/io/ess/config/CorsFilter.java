8853430eebb0ecaeab33720a214b2a8ca55af07f
Jonathan Miller <jonmiller44@gmail.com>
2017-02-05 23:18:10 -0500

    Changed CORS filter to allow only the webpad.essolutions.io origin

--- src/main/java/io/ess/config/CorsFilter.java
+++ src/main/java/io/ess/config/CorsFilter.java
@@ -31,7 +31,7 @@ public class CorsFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin", "webpad.essolutions.io");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, X-XSRF-TOKEN");
