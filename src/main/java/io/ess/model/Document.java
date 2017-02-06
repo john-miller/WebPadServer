f97a5d9cbccad957419b37bf694e0c21110fb4d6
Jonathan Miller <jonmiller44@gmail.com>
2017-02-05 23:19:28 -0500

    Added document model object

---
+++ src/main/java/io/ess/model/Document.java
@@ -0,0 +1,61 @@
+package io.ess.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Document {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String name;
+	private String content;
+	private Date createdOn;
+	private Date lastUpdatedOn;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	
+	public Date getLastUpdatedOn() {
+		return lastUpdatedOn;
+	}
+	
+	public void setLastUpdatedOn(Date lastUpdatedOn) {
+		this.lastUpdatedOn = lastUpdatedOn;
+	}
+	
+}
